7c2771ccb8e856b4c683517c044323918f8a7dc3
IvanGuGon1 <certified.courses@outoook.es>
2023-02-15 19:17:29 +0100

    week4 folder and bash etls

---
+++ week4/etl_fhv_2019_2020.sh
@@ -0,0 +1,6 @@
+array=(01 02 03 04 05 06 07 08 09 10 11 12)
+for i in "${array[@]}"
+do
+    wget -c "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/fhv_tripdata_2019-"$i".csv.gz"
+    gsutil cp "fhv_tripdata_2019-"$i".csv.gz" gs://prefect-zoomcamp-ivang/data/fhv
+done
---
+++ week4/etl_yellow_2019_2020.sh
@@ -0,0 +1,7 @@
+array=(01 02 03 04 05 06 07 08 09 10 11 12)
+for i in "${array[@]}"
+do
+    wget -c "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-"$i".csv.gz"
+
+    gsutil cp "yellow_tripdata_2019-"$i".csv.gz" gs://prefect-zoomcamp-ivang/data/yellow
+done
